145b90d135f53a5723a744208dea6743da8c22df
André Salazar <71437593+AndreSC1218@users.noreply.github.com>
2023-02-07 10:32:50 +0100

    ADD: Clase Coche
    se añadió la clase Coche

---
+++ EjerciciosTema3/src/ejerciciostema3/Coche.java
@@ -0,0 +1,17 @@
+package ejerciciostema3;
+
+public class Coche {
+  private int numPuertas;
+
+  public Coche(int numPuertas) {
+    this.numPuertas = numPuertas;
+  }
+
+  public void agregarPuerta() {
+    this.numPuertas++;
+  }
+
+  public int getNumPuertas() {
+    return this.numPuertas;
+  }
+}
