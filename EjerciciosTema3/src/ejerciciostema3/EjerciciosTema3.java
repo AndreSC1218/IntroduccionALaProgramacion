60ddcba453c4ff8df8f86298b82fee4267e368b7
André Salazar <71437593+AndreSC1218@users.noreply.github.com>
2023-02-07 10:37:08 +0100

    ADD: Crear objeto y usar sus funciones
    creamos un objeto tipo coche y usamos su función de agregar una puerta

--- EjerciciosTema3/src/ejerciciostema3/EjerciciosTema3.java
+++ EjerciciosTema3/src/ejerciciostema3/EjerciciosTema3.java
@@ -9,6 +9,14 @@ public class EjerciciosTema3 {
     //llamamos a la funcion y le damos valores
     int resultado = suma(5, 7, 3);
     System.out.println("La suma de los 3 numeros es: " + resultado);
+    
+    //Creamos un objeto coche
+    Coche miCoche = new Coche(2);
+    System.out.println("El numero de puertas es: " + miCoche.getNumPuertas());
+    
+    //Agregamos una puerta
+    miCoche.agregarPuerta();
+    System.out.println("El nuevo numero de puertas es: " + miCoche.getNumPuertas());
     }
     
 }
